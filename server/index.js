f7bc85b5d71124930657d298004414ebf08313ab
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-03-21 15:12:14 +0200

    Types fixed

--- server/index.js
+++ server/index.js
@@ -1,13 +1,33 @@
-const express = require('express')
-const cors = require('cors')
-const { Pool } = require('pg')
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
 
-const app = express()
-app.use(cors())
-const port = 3001
+const app = express();
+app.use(cors());
+const port = 3001;
 
-app.get("/", (req,res) => {
-  res.status(200).json({result: 'success'})
+app.get('/',(req, res) => {
+  const pool = openDb()
+
+  pool.query('select * from task', (error, result) => {
+    if (error) {
+      res.status(500).json({error: error.message})
+    }
+    res.status(200).json(result.rows)
+  })
 })
 
+const openDb = () => {
+  const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'todo',
+    password: 'root',
+    port: 5432
+  })
+  return pool
+}
+
 app.listen(port)
+
+
