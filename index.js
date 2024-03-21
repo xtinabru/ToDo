9a26d21ce348b5644d45a0d4256c8e46921f2d85
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-03-21 19:22:52 +0200

    I added renderTask to index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const list = document.querySelector('ul')
 const input = document.querySelector('input')
 
+const renderTask = (task) => {
+  const li = document.createElement('li')
+  li.setAttribute('class', 'list-group-item')
+  li.innerHTML = task
+  list.append(li)
+}
+
 input.addEventListener('keypress',(event) => {
   if(event.key === 'Enter'){
     event.preventDefault()
     const task = input.value.trim()
     if (task !== ''){
-      const li = document.createElement('li')
-      li.setAttribute('class', 'list-group-item')
-      li.innerHTML = task
-      list.append(li)
+      renderTask(task)
       input.value = ''
     }
 
