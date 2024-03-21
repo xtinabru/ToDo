063e6295215d8c45f78f851dd9b08eaef69f0198
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-03-21 23:21:28 +0200

    Added the function tp fetch data from backend by making HTTP call and receiveing JSONs

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ const renderTask = (task) => {
   list.append(li)
 }
 
+const getTask = async () => {
+  try {
+    const response = await fetch(BACKEND_ROOT_URL)
+    const json = await response.json()
+    json.forEach(task => {
+      renderTask(task.description)
+    })
+    input.disabled = false
+  } catch (error) {
+    alert("Error retrieving tasks " + error.message)
+  }
+}
+const saveTask = async (task) => {
+  saveTask(task).then((json)=>{
+    renderTask(task)
+    input.value = ''
+  })
+}
+
 input.addEventListener('keypress',(event) => {
   if(event.key === 'Enter'){
     event.preventDefault()
@@ -23,3 +42,5 @@ input.addEventListener('keypress',(event) => {
 
   }
 })
+
+getTask()
