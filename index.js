dcc501ea75db490ce838e82a5518586af25cdc12
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-03-21 19:28:08 +0200

    I added input.disabled

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const BACKEND_ROOT_URL = 'http://localhost:3001'
 const list = document.querySelector('ul')
 const input = document.querySelector('input')
 
+input.disabled = true
+
 const renderTask = (task) => {
   const li = document.createElement('li')
   li.setAttribute('class', 'list-group-item')
