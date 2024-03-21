960680893576fdf39b927fac8e22495609715dd3
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-03-21 18:28:27 +0200

    I added pool.query

--- server/index.js
+++ server/index.js
@@ -4,8 +4,23 @@ const { Pool } = require('pg');
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json)
 const port = 3001;
+
+app.post("/new", (req, res) =>{
+  const pool = openDb()
+
+  pool.query('insert into task (description) values ($1) returning *',
+  [req.body.description],
+  (error, result) => {
+    if (error){
+      res.status(500).json({error: error.message})
+    } else {
+      res.status(200).json({id: result.rows[0].id})
+    }
+  })
+})
+
 
 app.get('/',(req, res) => {
   const pool = openDb()
