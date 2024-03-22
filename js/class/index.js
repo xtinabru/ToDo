c22e38dc29b3e1857e36512ad2b772f184722128
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-03-22 23:13:31 +0200

    moved index.js to js folder

---
+++ js/class/index.js
@@ -0,0 +1,56 @@
+const BACKEND_ROOT_URL = 'http://localhost:3001'
+
+const list = document.querySelector('ul')
+const input = document.querySelector('input')
+
+input.disabled = true
+
+const renderTask = (task) => {
+  const li = document.createElement('li')
+  li.setAttribute('class', 'list-group-item')
+  li.innerHTML = task
+  list.append(li)
+}
+
+const getTask = async () => {
+  try {
+    const response = await fetch(BACKEND_ROOT_URL)
+    const json = await response.json()
+    json.forEach(task => {
+      renderTask(task.description)
+    })
+    input.disabled = false
+  } catch (error) {
+    alert("Error retrieving tasks " + error.message)
+  }
+}
+const saveTask = async (task) => {
+  try {
+    const json = JSON.stringify({description: task})
+    const response = await fetch(BACKEND_ROOT_URL + '/new',{
+      method: 'post',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: json
+    })
+    return response.json()
+  } catch (error) {
+    alert("Error saving task" + error.message)
+  }
+}
+
+input.addEventListener('keypress',(event) => {
+  if(event.key === 'Enter'){
+    event.preventDefault()
+    const task = input.value.trim()
+    if (task !== ''){
+      saveTask(task).then((json)=>{
+        renderTask(task)
+        input.value = ''
+      })
+     }
+    }
+  })
+
+getTask()
