65ba946fc97d8aef9834213d1781be73860aabf2
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-03-23 17:55:34 +0200

    defined the parametres for  const.app

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const { Pool } = require('pg');
 const app = express();
 app.use(cors());
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 const port = 3001;
 
 app.post("/new", (req, res) =>{
