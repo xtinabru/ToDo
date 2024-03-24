1b0ecf76142793298e5243a2bba7bb65af565e55
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-03-24 14:37:11 +0200

    modified app.delete

--- server/index.js
+++ server/index.js
@@ -38,17 +38,16 @@ app.get('/', async (req, res) => {
 })
 
 app.delete("/delete/:id", async(req,res) => {
-  const pool = openDb()
-  const id = parseInt(req.params.id)
-  pool.query('delete from task where id = $1',
-  [id],
-  (error, result) => {
-    if (error) {
-      res.status(500).json({error: error.message})
-    } else {
-      res.status(200).json({id:id})
-    }
-  })
+const id = Number(req.params.id)
+try {
+  const result = await query('delete from task where id = $1',
+  [id])
+  res.status(200).json({id:id})
+} catch (error) {
+  console.log(error)
+  res.statusMessage = error
+  res.status(500).json({error: error})
+}
 })
 
 const openDb = () => {
