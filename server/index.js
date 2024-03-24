8b3756d4b5fc971d9fc32567f5d6758c6f0b2b7c
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-03-24 14:01:16 +0200

    !

--- server/index.js
+++ server/index.js
@@ -56,7 +56,7 @@ const openDb = () => {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT
   })
   return pool
 }
