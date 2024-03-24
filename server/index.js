d81a3555a7d6c2f8eaf58b712abe19af4b244d9f
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-03-24 19:43:08 +0200

    7!

--- server/index.js
+++ server/index.js
@@ -1,56 +1,20 @@
 require('dotenv').config()
 
+const express = require('express')
+const cors = require('cors')
+const { todoRouter } = require('./routes/todo.js')
+//const { Pool } = require('pg');
+//const { query } = require('./helpers/db')
 
-const express = require('express');
-const cors = require('cors');
-const { Pool } = require('pg');
-const { query } = require('./helpers/db')
-
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use('/', todoRouter)
+
 const port = process.env.PORT;
 
-app.post("/new", async (req, res) =>{
-try {
-  const result = await query('insert into task (description) values ($1) returning *', [req.body.description])
-  res.status(200).json({id:result.rows[0].id})
-}catch (error) {
-  console.log(error)
-  res.statusMessage = error
-  res.status(500).json({error: error})
-}
-})
-
-
-app.get('/', async (req, res) => {
-  console.log(query)
-  try {
-    const result = await query('select * from task')
-    const rows = result.rows ? result.rows : []
-    res.status(200).json(rows)
-  }catch (error) {
-    console.log(error)
-    res.statusMessage = error
-    res.status(500).json({error: error})
-  }
-})
-
-app.delete("/delete/:id", async(req,res) => {
-  const id = Number(req.params.id)
-  try {
-    const result = await query ('delete from task where id = $1',[id])
-    res.status(200).json({id:id})
-  }catch (error) {
-    console.log(error)
-    res.statusMessage = error
-    res.status(500).json({error: error})
-  }
-})
-
-
-
 app.listen(port)
-
-
+  
+  
+  
